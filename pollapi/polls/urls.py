32348c8901eba90ef7be86bf636a7e25824150e2
aman <KAN007BCT@student.kec.edu.np>
2020-12-20 22:17:55 +0545

    upto chapter 5

--- pollapi/polls/urls.py
+++ pollapi/polls/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-	path('polls/', views.polls_list, name='polls-list'),
-	path('polls/<int:pk>/', views.polls_detail, name='polls-detail'),		
+	path('polls/', views.PollList.as_view(), name='polls-list'),
+	path('polls/<int:pk>/', views.PollDetail.as_view(), name='polls-detail'),
+	path('choices/', views.ChoiceList.as_view(), name='choice-list'),
+	path('vote/', views.CreateVote.as_view(), name='create-view'),		
 ]
--- pollapi/polls/views.py
+++ pollapi/polls/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Choice, Poll, Vote
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from .models import Poll, Choice
+from rest_framework.views import APIView
+from rest_framework import generics
+
 # Create your views here.
 
+
+#class PollList(APIView):
+#	def get(self, request):
+#		polls = Poll.objects.all()[:20]
+#		serializer = PollSerializer(polls, many=True)
+#		return Response(serializer.data)
+
+
+#class PollDetail(APIView):
+#	def get(self, request, pk):
+#		poll = get_object_or_404(Poll, pk=pk)
+#		serializer = PollSerializer(poll)
+#		return Response(serializer.data)
 
 def polls_list(request):
 	MAX_OBJECTS = 20
@@ -20,3 +39,18 @@ def polls_detail(request, pk):
 	}
 	return JsonResponse(data)
 
+class PollList(generics.ListCreateAPIView):
+	queryset = Poll.objects.all()
+	serializer_class = PollSerializer
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+	queryset = Poll.objects.all()
+	serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+	queryset = Choice.objects.all()
+	serializer_class = ChoiceSerializer
+
+class CreateVote(generics.CreateAPIView):
+	serializer_class = VoteSerializer
